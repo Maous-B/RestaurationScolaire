3438bdb2b1c85d09e303008c52acff4c0ceee5ef
Maous-B <maousgaming01@gmail.com>
2024-03-18 12:10:45 +0100

    Modif 18 mars

--- mharnafi/laravel-projet/SituationProUn/database/migrations/2024_03_12_075248_create_reservations_table.php
+++ mharnafi/laravel-projet/SituationProUn/database/migrations/2024_03_12_075248_create_reservations_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('date');
             $table->timestamps();
         });
