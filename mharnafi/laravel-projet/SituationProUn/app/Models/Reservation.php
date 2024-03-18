be96b30c0b108d2f3f860b8119f0d35e02fc67fe
Maous-B <maousgaming01@gmail.com>
2024-03-18 11:16:30 +0100

    Après modif jusqu'au 18 mars 2024

--- mharnafi/laravel-projet/SituationProUn/app/Models/Reservation.php
+++ mharnafi/laravel-projet/SituationProUn/app/Models/Reservation.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', // L'ID de l'utilisateur associé à la réservation
+        'date',    // La date de la réservation
+        // Ajoute d'autres colonnes si nécessaire
+    ];
+
+    // Relation avec le modèle User (chaque réservation appartient à un utilisateur)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
